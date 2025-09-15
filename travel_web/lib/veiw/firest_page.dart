9d5ed464f22250563a16687369010435749afcb7
jihokim1 <kig0128@naver.com>
2025-09-15 15:23:28 +0900

    Web관리자 로그인

---
+++ travel_web/lib/veiw/firest_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class FirestPage extends StatelessWidget {
+  const FirestPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
